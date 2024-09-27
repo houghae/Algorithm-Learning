94662ec1bedf1f1da4874c1ef4b753f093cf72cd
Arthur Hough <hough1ae@gmail.com>
2024-09-27 08:42:18 -0600

    edit quick sort file

--- quicksort.py
+++ quicksort.py
@@ -1,5 +1,7 @@
-
 def quicksort(values):
+  """
+  Recursive sorting algorithm that iterates though a list starting at the beginning.
+  """
   if len(values) <= 1:
     return values
   less_than_pivot = []
@@ -10,8 +12,17 @@ def quicksort(values):
       less_than_pivot.append(value)
     else:
       greater_than_pivot.append(value)
-  return quicksort(less_than_pivot) + [pivot] +
-    quicksort(greater_than_pivot)
+  return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
 
-sorted_numbers = quicksort(numbers)
-print(sorted_numbers)
+
+random_list = [3,11,77,44,21,150,100003,765432,34,67,6,5]
+
+
+def main():
+  print(random_list)
+  sorted_list = quicksort(random_list)
+  print(sorted_list)
+
+
+if __name__=="__main__":
+  main()
